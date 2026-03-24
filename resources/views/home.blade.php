e1056ebb5c70eb33bd01bbaf0e97dbc724f0366c
Tudor Chelaru <tudorchelaru@yahoo.com>
2026-03-24 18:20:33 +0200

    chore: auto-save 2026-03-24 18:20 - modificari locale necomise

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>PFA Expenses — Registrul tău, fără bătăi de cap</title>
+<title>Nuva.ro — Evidență financiară pentru profesioniști independenți</title>
 <link rel="icon" type="image/svg+xml" href="/favicon.svg">
 <link rel="shortcut icon" href="/favicon.ico">
 <link rel="preconnect" href="https://fonts.googleapis.com">
